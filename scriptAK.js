6a768f774472b026739e85f9e381242c63d7b956
alku008 <94560317+alku008@users.noreply.github.com>
2022-01-03 20:22:44 +0000

    Deploying to gh-pages from master @ 5e1b3997adce14c6f9c396519f743e639c2b1a23 🚀

--- scriptAK.js
+++ scriptAK.js
@@ -1,4 +1,23 @@
+let helloWorldPopup;
 WA.onInit().then(() => {
     console.log('Current player name: ', WA.player.name);
-	WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+	//WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+	
+
+	// Open the popup when we enter a given zone
+	helloWorldPopup = WA.room.onEnterLayer("silentzone").subscribe(() => {
+		WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+			label: "Close",
+			className: "primary",
+			callback: (popup) => {
+				// Close the popup when the "Close" button is pressed.
+				popup.close();
+			}
+		}]);
+	});
+
+	// Close the popup when we leave the zone.
+	WA.room.onLeaveLayer("silentzone").subscribe(() => {
+		helloWorldPopup.close();
+	});
 });
