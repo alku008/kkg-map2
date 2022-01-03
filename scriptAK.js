ba818f221dabd1d2af1b24f1336a375b9946ea81
alku008 <94560317+alku008@users.noreply.github.com>
2022-01-03 22:28:48 +0000

    Deploying to gh-pages from master @ 36d85e83534d37c5f8dcbc1555b4c02c67ac7fac 🚀

--- scriptAK.js
+++ scriptAK.js
@@ -24,10 +24,12 @@ WA.onInit().then(() => {
 	
 	// Open the popup when we enter a given zone
 	helloWorldPopup = WA.room.onEnterLayer("eingang").subscribe(() => {
+		WA.controls.disablePlayerControls();
 		WA.ui.openPopup("popupEingang", 'Die lila Flächen sind Meeting Flächen!', [{
 			label: "schliessen",
 			className: "primary",
 			callback: (popup) => {
+				WA.controls.restorePlayerControls();
 				// Close the popup when the "Close" button is pressed.
 				popup.close();
 			}
